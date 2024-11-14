16a76329d63d3812f96b17632fbbe16aaafde862
lashu333 <140723330+lashu333@users.noreply.github.com>
2024-11-14 13:26:48 +0400

    Initial Commit

---
+++ Dashboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dashboard
+//
+//  Created by Lasha Tavberidze on 14.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dashboard/DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardApp.swift
+//  Dashboard
+//
+//  Created by Lasha Tavberidze on 14.11.24.
+//
+
+import SwiftUI
+
+@main
+struct DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
